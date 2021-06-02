0b1596bca6fbf97c0b1d7112eecb7c496527e307
Headshot20072007@mail.ru <headshot20072007@mail.ru>
2021-06-02 12:35:47 +0300

    Notepad v.1, working

--- post.rb
+++ post.rb
@@ -1,28 +1,43 @@
 class Post
   def initialize
-    @created_at = Time.now
-    @text = nill
+    @created_at = Time.now # дата создания записи
+    @text = [] # массив строк записи — пока пустой
   end
-  def read_from_console
 
+  def self.post_types
+    [Memo, Task, Link]
+  end
+
+  def self.create(type_index)
+    return post_types[type_index].new
+  end
+
+  def read_from_console
+    # todo: должен реализовываться детьми, которые знают как именно считывать свои данные из консоли
   end
 
   def to_strings
-
+    # todo: должен реализовываться детьми, которые знают как именно хранить себя в файле
   end
 
+  # Записывает текущее состояние объекта в файл
   def save
-    file = File.new(file_path)
+    file = File.new(file_path, "w:UTF-8") # открываем файл на запись
 
-    for item in to_strings do
+    for item in to_strings do # идем по массиву строк, полученных из метода to_strings
       file.puts(item)
     end
-    file.close
+
+    file.close # закрываем
   end
+
+  # Метод, возвращающий путь к файлу, куда записывать этот объект
   def file_path
-    current_path = File.dirman(__FILE__ )
+    current_path = File.dirname(__FILE__)
+
     file_name = @created_at.strftime("#{self.class.name}_%Y-%m-%d_%H-%M-%S.txt")
 
     return current_path + "/" + file_name
   end
 end
+
